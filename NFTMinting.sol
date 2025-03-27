97379bd822f6b8fb07dfa89cd1c0951f8a32f64e
trishhaaa24 <136618149+trishhaaa24@users.noreply.github.com>
2025-03-28 00:34:36 +0530

    Add files via upload
    Initial Commit - "NFT Minting Smart Contract Deployment"

---
+++ NFTMinting.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTMinting is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+    mapping(uint256 => bool) private _mintedTokens; 
+
+    constructor(address initialOwner) ERC721("MyNFT", "MNFT") Ownable(initialOwner) {}
+
+    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+        _tokenIdCounter++;
+        uint256 newTokenId = _tokenIdCounter;
+
+        _safeMint(recipient, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        _mintedTokens[newTokenId] = true; // ✅ Track token existence
+
+        return newTokenId;
+    }
+
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        require(_mintedTokens[tokenId], "Token does not exist"); // ✅ Alternative check
+        return tokenURI(tokenId);
+    }
+}
